ddf4f123e2f5781db4bdaa7b689a20f8aca596bb
hyunsuklee <lhs5323@gmail.com>
2022-02-10 19:48:34 +0900

    Contest 770 Problem B Java

--- Contest770/Java/FortuneTelling.java
+++ Contest770/Java/FortuneTelling.java
@@ -9,6 +9,17 @@ public class FortuneTelling{
     PrintWriter pw = new PrintWriter(System.out);
     int t = Integer.parseInt(br.readLine());
     while(t-- > 0){
+      long[] test = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+      long[] arr = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+      for(int i = 0; i < test[0]; i++){
+        test[1] += arr[i];
+      }
+      if((test[1] - test[2]) % 2 == 0){
+        pw.println("Alice");
+      } else{
+        pw.println("Bob");
+      }
     }
+    pw.close();
   }
 }
