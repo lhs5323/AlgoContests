2d59ee592d63df6ebae9c5eb46c74cffe8d7a47f
hyunsuklee <lhs5323@gmail.com>
2022-04-09 20:01:37 +0900

    Contest 781 Problem B

--- Contest781/Java/Cloning.java
+++ Contest781/Java/Cloning.java
@@ -13,7 +13,7 @@ public class Cloning{
       int i = Integer.parseInt(br.readLine());
       int[] test = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
       int ans = 0;
-      HashMap<Integer, Integer> map = new HashMap<Integer>();
+      Map<Integer, Integer> map = new HashMap<Integer, Integer>();
       for(int num : test){
         if(!map.containsKey(num)){
           map.put(num, 1);
@@ -21,47 +21,18 @@ public class Cloning{
           map.put(num, map.get(num) + 1);
         }
       }
-      int max = getMaxEntryInMapBasedOnKey(map);
-      if(i == 2 && max == 1){
-        ans = 1;
-      } else if(i == 3 && max == 1){
-        ans = 4;
-      } else if(i == 4 && max == 1){
-        ans = 5;
-      } else if(i > 4 && max == 1){
-        ans = i + 2;
-      } else
+
+				int max = Collections.max(map.values());
+				int cnt = i - max;
+				while(max < i) {
+					ans++;
+					max <<= 1;
+				}
+        pw.println(cnt + ans);
 
 
 
     }
+    pw.close();
   }
-  public static <K extends Comparable<K>, V> Map.Entry<K, V>
-    getMaxEntryInMapBasedOnKey(Map<K, V> map)
-    {
-        // To store the result
-        Map.Entry<K, V> entryWithMaxKey = null;
-
-        // Iterate in the map to find the required entry
-        for (Map.Entry<K, V> currentEntry : map.entrySet()) {
-
-            if (
-
-                // If this is the first entry,
-                // set the result as this
-                entryWithMaxKey == null
-
-                // If this entry's key is more than the max key
-                // Set this entry as the max
-                || currentEntry.getKey()
-                           .compareTo(entryWithMaxKey.getKey())
-                       > 0) {
-
-                entryWithMaxKey = currentEntry;
-            }
-        }
-
-        // Return the entry with highest key
-        return entryWithMaxKey;
-    }
 }
