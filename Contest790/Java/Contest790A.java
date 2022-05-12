31863e51e90ec778d0073a781364a58304280ffa
hyunsuklee <lhs5323@gmail.com>
2022-05-12 23:06:27 +0900

    [Prep] Contest790

---
+++ Contest790/Java/Contest790A.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Contest790A{
+  public static PrintWriter pw = new PrintWriter(System.out);
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    PrintWriter pw = new PrintWriter(System.out);
+    long t = Integer.parseInt(br.readLine());
+    while(t-- > 0){
+    }
+  }
+}
